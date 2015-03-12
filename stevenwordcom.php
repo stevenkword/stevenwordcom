1b98abe1a82e1658cc58ddcc5d7275269d2d4bfd
Steven K Word <stevenword@gmail.com>
2015-03-12 16:10:42 -0700

    Color sub logics

--- stevenwordcom.php
+++ stevenwordcom.php
@@ -23,20 +23,126 @@ function s8d_filter_bloginfo_description( $bloginfo, $show ) {
 
 		if( ! is_front_page() && ( is_category( 'blog' ) || ( is_single() && in_category( 'blog', $post ) ) ) ) {
 			$bloginfo = str_replace( "creating things", "<u>all</u> the things", $bloginfo );
-		}
-
-		if( ! is_front_page() && ( is_category( 'wordpress' ) || ( is_single() && in_category( 'wordpress', $post ) ) ) ){
+		} else if ( ! is_front_page() && ( is_category( 'wordpress' ) || ( is_single() && in_category( 'wordpress', $post ) ) ) ){
 			$bloginfo = str_replace( "creating things", "WordPress", $bloginfo );
-		}
-
-		if( ! is_front_page() && ( is_category( 'hiking' ) || ( is_single() && in_category( 'hiking', $post ) ) ) ) {
+		} elseif ( ! is_front_page() && ( is_category( 'hiking' ) || ( is_single() && in_category( 'hiking', $post ) ) ) ) {
 			$bloginfo = str_replace( "creating things", "climbing things", $bloginfo );
-		}
-
-		if( ! is_front_page() && ( is_category( 'cars' ) || ( is_single() && in_category( 'cars', $post ) ) ) ) {
+		} elseif ( ! is_front_page() && ( is_category( 'cars' ) || ( is_single() && in_category( 'cars', $post ) ) ) ) {
 			$bloginfo = str_replace( "creating things", "wrenching on things", $bloginfo );
 		}
 	}
 	return $bloginfo;
 }
 add_filter( 'bloginfo', 's8d_filter_bloginfo_description', 10, 2 );
+
+
+/**
+ * Change styles based on category
+ */
+function s8d_filter_wp_head_override() {
+
+	$lcol_color   = '';
+	$rcol_color   = '';
+	$header_color = '';
+
+	global $post;
+
+	if( ! is_front_page() && ( is_category( 'blog' ) || ( is_single() && in_category( 'blog', $post ) ) ) ) {
+
+	} else if ( ! is_front_page() && ( is_category( 'wordpress' ) || ( is_single() && in_category( 'wordpress', $post ) ) ) ){
+
+	} elseif ( ! is_front_page() && ( is_category( 'hiking' ) || ( is_single() && in_category( 'hiking', $post ) ) ) ) {
+		$lcol_color   = '#89A894';
+		$rcol_color   = '#8A9574';
+		$header_color = '#4d4d4c';
+	} elseif ( ! is_front_page() && ( is_category( 'cars' ) || ( is_single() && in_category( 'cars', $post ) ) ) ) {
+
+	}
+	?>
+	<style type="text/css">
+	/* Dynamic CSS: For no styles in head, copy and put the css below in your child theme's style.css, disable dynamic styles */
+
+	::selection { background-color: <?php echo $lcol_color; ?>; }
+	::-moz-selection { background-color: <?php echo $lcol_color; ?>; }
+
+	a,
+	.themeform label .required,
+	#flexslider-featured .flex-direction-nav .flex-next:hover,
+	#flexslider-featured .flex-direction-nav .flex-prev:hover,
+	.post-hover:hover .post-title a,
+	.post-title a:hover,
+	.s1 .post-nav li a:hover i,
+	.content .post-nav li a:hover i,
+	.post-related a:hover,
+	.s1 .widget_rss ul li a,
+	#footer .widget_rss ul li a,
+	.s1 .widget_calendar a,
+	#footer .widget_calendar a,
+	.s1 .alx-tab .tab-item-category a,
+	.s1 .alx-posts .post-item-category a,
+	.s1 .alx-tab li:hover .tab-item-title a,
+	.s1 .alx-tab li:hover .tab-item-comment a,
+	.s1 .alx-posts li:hover .post-item-title a,
+	#footer .alx-tab .tab-item-category a,
+	#footer .alx-posts .post-item-category a,
+	#footer .alx-tab li:hover .tab-item-title a,
+	#footer .alx-tab li:hover .tab-item-comment a,
+	#footer .alx-posts li:hover .post-item-title a,
+	.comment-tabs li.active a,
+	.comment-awaiting-moderation,
+	.child-menu a:hover,
+	.child-menu .current_page_item > a,
+	.wp-pagenavi a { color: <?php echo $lcol_color; ?>; }
+
+	.themeform input[type="submit"],
+	.themeform button[type="submit"],
+	.s1 .sidebar-top,
+	.s1 .sidebar-toggle,
+	#flexslider-featured .flex-control-nav li a.flex-active,
+	.post-tags a:hover,
+	.s1 .widget_calendar caption,
+	#footer .widget_calendar caption,
+	.author-bio .bio-avatar:after,
+	.commentlist li.bypostauthor > .comment-body:after,
+	.commentlist li.comment-author-admin > .comment-body:after { background-color: <?php echo $lcol_color; ?>; }
+
+	.post-format .format-container { border-color: <?php echo $lcol_color; ?>; }
+
+	.s1 .alx-tabs-nav li.active a,
+	#footer .alx-tabs-nav li.active a,
+	.comment-tabs li.active a,
+	.wp-pagenavi a:hover,
+	.wp-pagenavi a:active,
+	.wp-pagenavi span.current { border-bottom-color: <?php echo $lcol_color; ?> !important; }
+
+
+	.s2 .post-nav li a:hover i,
+	.s2 .widget_rss ul li a,
+	.s2 .widget_calendar a,
+	.s2 .alx-tab .tab-item-category a,
+	.s2 .alx-posts .post-item-category a,
+	.s2 .alx-tab li:hover .tab-item-title a,
+	.s2 .alx-tab li:hover .tab-item-comment a,
+	.s2 .alx-posts li:hover .post-item-title a { color: <?php echo $rcol_color; ?>; }
+
+	.s2 .sidebar-top,
+	.s2 .sidebar-toggle,
+	.post-comments,
+	.jp-play-bar,
+	.jp-volume-bar-value,
+	.s2 .widget_calendar caption { background-color: <?php echo $rcol_color; ?>; }
+
+	.s2 .alx-tabs-nav li.active a { border-bottom-color: <?php echo $rcol_color; ?>; }
+	.post-comments span:before { border-right-color: <?php echo $rcol_color; ?>; }
+
+
+	#header { background-color: <?php echo $header_color; ?>; }
+	@media only screen and (min-width: 720px) {
+		#nav-header .nav ul { background-color: <?php echo $header_color; ?>; }
+	}
+
+	#footer-bottom { background-color: <?php echo $header_color; ?>; }
+	</style>
+	<?php
+}
+add_filter( 'wp_head', 's8d_filter_wp_head_override', 999, 2 );
